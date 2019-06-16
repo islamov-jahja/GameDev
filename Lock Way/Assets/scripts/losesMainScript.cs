cb721aa7720c93f6646d089d959444656ba0015b
yahya <yahyaislamov15@mail.ru>
2019-06-16 17:55:37 +0300

    сделла некоторые изменения

---
+++ Lock Way/Assets/scripts/losesMainScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.scripts;
+using TMPro;
+
+public class losesMainScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI timeText;
+    void Start()
+    {
+        timeText.text = GameManager.GetInstance().minutesResult + ":" + GameManager.GetInstance().secondsResult;
+    }
+}
---
+++ Lock Way/Assets/scripts/reloadInLose.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.scripts;
+using UnityEngine.SceneManagement;
+
+public class reloadInLose : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        GameManager gameManager = GameManager.GetInstance();
+        SceneManager.LoadScene(gameManager.lastLevel);
+    }
+}
